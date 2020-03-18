f231a5bf7fa188f7936f79ca1f2350718a7e06cc
akash0289 <akashshri171@gmail.com>
2020-03-18 17:53:12 +0530

    scroll new logic

--- src/main/java/practise/AppiumFramework/Utilities.java
+++ src/main/java/practise/AppiumFramework/Utilities.java
@@ -1,5 +1,6 @@
 package practise.AppiumFramework;
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
@@ -15,5 +16,18 @@ public class Utilities
 	{
 		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\""+text+"\"));");
 	}
-
+	
+	//scroll whole page
+/* MobileElement el = (MobileElement) driver
+					    .findElementByAndroidUIAutomator("new UiScrollable("
+					        + "new UiSelector().scrollable(true)).scrollIntoView("                      
+					        + "new UiSelector().textContains(\"Country of Play School\"));");
+			 // el.click();
+			
+			 //scroll when resourceId is known 
+			   MobileElement elementToClick = (MobileElement) driver
+					    .findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()"
+					        + ".resourceId(\"android:id/content\")).scrollIntoView("
+					        + "new UiSelector().text(\"textName\"));");
+					//elementToClick.click();*/
 }
